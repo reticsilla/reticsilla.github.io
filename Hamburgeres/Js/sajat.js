719a161580981b82a5881e4ff2ef278466f58ebe
reticsilla <reticsilla@gmail.com>
2020-07-11 17:20:58 +0200

    20200711

--- Hamburgeres/Js/sajat.js
+++ Hamburgeres/Js/sajat.js
@@ -1,10 +1,6 @@
-function calcAmount() {
+function nevEllenorzes() {
     const nev = document.querySelector("input[name='name']").value.trim();
-    const email = document.querySelector("input[name='email']").value.trim();
-    const cim = document.querySelector("input[name='address']").value.trim();
     let hnev="";
-    let hemail="";
-    let hcim="";
     if (!nev) {
         hnev="A név megadása kötelező!";
     }
@@ -12,6 +8,12 @@ function calcAmount() {
             hnev="";
         };
     document.getElementById("hibanev").innerHTML = hnev;
+    emailEllenorzes();
+}
+
+function emailEllenorzes() {
+    const email = document.querySelector("input[name='email']").value.trim();
+    let hemail="";
     if (!email || !(email.indexOf('@')>0) || !(email.indexOf('.')>0)) {
         hemail="Az e-mail cím hiányzik, vagy érvénytelen! Helyesen adja meg az email címét!";
     }
@@ -19,6 +21,12 @@ function calcAmount() {
             hemail="";
         }
     document.getElementById("hibaemail").innerHTML = hemail;
+    cimEllenorzes();
+}
+
+function cimEllenorzes() {
+    const cim = document.querySelector("input[name='address']").value.trim();
+    let hcim="";
     if (!cim) {
         hcim="A szállítási cím megadása kötelező!";
     }
@@ -26,32 +34,56 @@ function calcAmount() {
             hcim="";
         }
     document.getElementById("hibacim").innerHTML = hcim;
-    let extra = 0;
-    let szosz = 0;
+    extraEllenorzes();
+}
+
+let extra = 0;
+function extraEllenorzes() {
     let echeese = document.getElementById("cheese").checked;
     let edouble = document.getElementById("double").checked;
     let emozarella = document.getElementById("mozarella").checked;
     let eparmesan = document.getElementById("parmesan").checked;
-    let esauce = document.getElementById("sauce").value;
     if (echeese > 0 || edouble > 0 || emozarella > 0 || eparmesan > 0) {extra=80};
-    if ((esauce === "Mustár") || (esauce === "Majonéz") || (esauce === "Ketchup")) {szosz=50};
-    let price = 1200;
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
-    let amountNumber = parseInt(amountInput.value);
+    szoszEllenorzes();
+    return (extra);
+}
+
+let szosz= 0;
+function szoszEllenorzes() {
+    let esauce = document.getElementById("sauce").value;
+    if ((esauce === "Mustár") || (esauce === "Majonéz") || (esauce === "Ketchup")) {szosz=50}
+        else {szosz=0};
+    darabEllenorzes();
+    return (szosz);
+}
+
+let amountInput = "";
+let amountNumber = 0;
+function darabEllenorzes() {
+    amountInput = document.querySelector("input[name='amount-input']");
+    amountNumber = parseInt(amountInput.value);
     if (isNaN(amountNumber)) {amountNumber=0};
     let hdarab="";
-    let amount=0;
     if (parseInt(amountNumber) > 10) {
         hdarab="Legfeljebb 10 terméket vásárolhat!";
+        amountNumber = 0;
     }
         else if ( parseInt(amountNumber) < 1) {
             hdarab="Legalább 1 terméket rendelnie kell!"
         }
         else {
-            hdarab="";
-            amount = parseInt(amountNumber) * (price + extra + szosz);
+            hdarab="";   
         }
-    showAmount.innerHTML = amount;
     document.getElementById("darab").innerHTML = hdarab;
+    calcAmount();
+    return (amountNumber);
+}
+
+let amount=0;
+let showAmount = document.querySelector("span.show-amount");
+function calcAmount() {
+    let price = 1200;
+    amount = parseInt(amountNumber) * (price + extra + szosz);
+    if (amount < 5000 && amountNumber >0 && amountNumber < 11) {amount = amount + 500};
+    showAmount.innerHTML = amount;  
 }
